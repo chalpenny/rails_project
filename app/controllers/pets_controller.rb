7679e546ca0168e2e130fb7d43071c4ebd9d5b01
chalpenny <cp.halpenny@gmail.com>
2020-01-18 18:46:34 -0800

    Build out Pet controller actions and start views

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,32 @@
 class PetsController < ApplicationController
+
+    def new
+        @pet = Pet.new
+        #pet = temp_pet_notes.build("senior")
+        #@pet = temp_pet_notes.build("allergies")
+    end
+
+    def create
+        @pet = Pet.new(pet_params)
+        if @pet.save
+            redirect_to @pet
+        else
+            render :new
+        end
+    end
+
+    def update
+
+    end
+
+    def show
+        @pet = Pet.find_by(params[:id])
+    end
+
+    private
+
+    def pet_params
+        params.require(:pet).permit(:name, :description, :notes, temp_pet_note_attributes: [:content])
+    end
+
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,5 @@
 class Pet < ApplicationRecord
   belongs_to :client
   has_many :temp_pet_notes
+  accepts_nested_attributes_for :temp_pet_notes
 end
